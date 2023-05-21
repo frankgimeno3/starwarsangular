40c8cbf4535a7dcdd6e34d578f4a3bc17ed2ab92
Frank Gimeno <frankgimeno3@gmail.com>
2023-05-21 19:49:31 +0200

    added some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     HttpClientModule
   ],
-  providers: [StarWarsService, LogService],
+  providers: [StarWarsService,LogService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/create-character/create-character.component.html',
+    './src/header/header.component.html',
+    './src/item/item.component.html',
+    './src/list/list.component.html',
+    './src/tabs/tabs.component.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
